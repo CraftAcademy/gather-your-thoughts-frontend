dd0703c1aeda0d61c997e508a11540b7b13a7341
aljoh <reachingalfred@gmail.com>
2018-02-02 14:19:53 +0100

    pie chart in progress

---
+++ src/data/data.ts
@@ -0,0 +1,7 @@
+export const StatsPieChart: any[] = [
+  {sentiment: "Happy", amount: 10},
+  {sentiment: "Sad", amount: 10},
+  {sentiment: "Excited", amount: 10},
+  {sentiment: "Angry", amount: 10},
+  {sentiment: "Disgusted", amount: 10}
+];
