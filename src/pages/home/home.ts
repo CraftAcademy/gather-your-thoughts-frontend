d62c17acc80b2ac075da98645818cb6fd2d9486b
aljoh <reachingalfred@gmail.com>
2018-02-05 13:46:50 +0100

    changes donut chart

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { ModalController, NavController, NavParams, ToastController } from 'ionic-angular';
 import { SentimentsProvider } from '../../providers/sentiments/sentiments';
 import { EntriesProvider } from '../../providers/entries/entries'
-import * as d3 from 'd3-selection';
-import * as d3Scale from "d3-scale";
-import * as d3Shape from "d3-shape";
 import { EntriesShowPage } from "../entries-show/entries-show";
 
 @Component({
@@ -14,22 +11,18 @@ import { EntriesShowPage } from "../entries-show/entries-show";
 
 
 export class HomePage {
-
-  title: string = 'D3 Pie Chart in Ionic 3';
-
-  margin = {top: 20, right: 20, bottom: 30, left: 50};
-  width: number;
-  height: number;
-  radius: number;
-  entries: any;
-
-  arc: any;
-  labelArc: any;
-  labelPer: any;
-  pie: any;
-  color: any;
-  svg: any;
   sentiments: any;
+  entries: any;
+  doughnutChartLabels:string[] = [];
+  doughnutChartData:number[] = [];
+  doughnutChartColors:any[] = [{
+    fillColor: 'rgba(47, 132, 71, 0.8)',
+    strokeColor: 'rgba(47, 132, 71, 0.8)',
+    highlightFill: 'rgba(47, 132, 71, 0.8)',
+    highlightStroke: 'rgba(47, 132, 71, 0.8)'
+  }];
+  doughnutChartType:string = 'doughnut';
+  isDataAvailable:boolean = false;
 
   constructor(public modalCtrl: ModalController,
               public sentimentsProvider: SentimentsProvider,
@@ -47,19 +40,27 @@ export class HomePage {
         toast.present();
       }
 
-
     this.sentimentsProvider.getMonthSentiments().subscribe((data) => {
       this.sentiments = data.sentiments.map((x : any) => ({sentiment: x.name, amount: x.taggings_count}));
-      this.initSvg();
-      this.drawPie();
+      for (let sentiment of this.sentiments) {
+        this.doughnutChartLabels.push(sentiment.sentiment);
+        this.doughnutChartData.push(sentiment.amount);
+        this.isDataAvailable = true;
+
+      }
     });
-    this.width = 900 - this.margin.left - this.margin.right ;
-    this.height = 500 - this.margin.top - this.margin.bottom;
-    this.radius = Math.min(this.width, this.height) / 2;
 
     this.entriesProvider.getRecentEntries().subscribe(({data}) => {
       this.entries = data.reverse();
     })
+  }
+
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
   }
 
   presentEntryModal() {
@@ -74,51 +75,6 @@ export class HomePage {
     this.navCtrl.push(EntriesShowPage, {
       id: entryId
     });
-  }
-
-  initSvg() {
-    this.color = d3Scale.scaleOrdinal()
-      .range(["#3366CC", "#DC3912", "#FF9900", "#109618", "#990099", "grey", "#00FA9A"]);
-    this.arc = d3Shape.arc()
-      .outerRadius(this.radius - 10)
-      .innerRadius(100);
-    this.labelArc = d3Shape.arc()
-      .outerRadius(this.radius - 40)
-      .innerRadius(this.radius - 40);
-
-    this.labelPer = d3Shape.arc()
-      .outerRadius(this.radius -80)
-      .innerRadius(this.radius -80);
-
-
-    this.pie = d3Shape.pie()
-      .sort(null)
-      .value((d: any) => d.amount);
-
-    this.svg = d3.select("#pieChart")
-      .append("svg")
-      .attr("width", '100%')
-      .attr("height", '100%')
-      .attr('viewBox','0 0 '+Math.min(this.width,this.height)+' '+Math.min(this.width,this.height))
-      .append("g")
-      .attr("transform", "translate(" + Math.min(this.width,this.height) / 2 + "," + Math.min(this.width,this.height) / 2 + ")");
-
-  }
-
-  drawPie() {
-    let g = this.svg.selectAll(".arc")
-      .data(this.pie(this.sentiments))
-      .enter().append("g")
-      .attr("class", "arc");
-    g.append("path").attr("d", this.arc)
-      .style("fill", (d: any) => this.color(d.data.sentiment) );
-    g.append("text").attr("transform", (d: any) => "translate(" + this.labelArc.centroid(d) + ")")
-      .attr("dy", ".35em")
-      .text((d: any) => d.data.sentiment);
-
-    g.append("text").attr("transform", (d: any) => "translate(" + this.labelPer.centroid(d) + ")")
-      .attr("dy", ".35em")
-      .text((d: any) => d.data.amount);
   }
 
 }
