de2ad675283988ffb7e2043d439a09ec3c022d61
aljoh <reachingalfred@gmail.com>
2018-02-05 01:09:14 +0100

    refactors naming from thought to entry

--- src/pages/entries-show/entries-show.module.ts
+++ src/pages/entries-show/entries-show.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { EntriesShowPage } from './Entries-show';
+import { EntriesShowPage } from './entries-show';
 
 @NgModule({
   declarations: [
--- src/pages/entry_modal/entry_modal.module.ts
+++ src/pages/entry_modal/entry_modal.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { EntryModalPage } from './Entry_modal';
+import { EntryModalPage } from './entry_modal';
 
 @NgModule({
   declarations: [
