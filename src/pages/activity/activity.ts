c7453bacc84bcabff94d7f665dee0b9f252613be
ScottB <17sb@cis.dk>
2018-02-06 14:19:57 +0100

    Add function in entries provider to get weeklyThoughts, display graph with correct data in component

--- src/pages/activity/activity.ts
+++ src/pages/activity/activity.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ActivityPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { EntriesProvider } from '../../providers/entries/entries';
 
 @IonicPage()
 @Component({
@@ -14,8 +8,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'activity.html',
 })
 export class ActivityPage {
+  entries: any;
+  weekdays:string[] = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  isDataAvailable: boolean = false;
+  barChartOptions:any = {
+  scaleShowVerticalLines: true,
+  responsive: true
+};
+  barChartLabels:string[] = [];
+  barChartType:string = 'bar';
+  barChartLegend:boolean = true;
+  barChartColors: any [] =[{ backgroundColor:'#89D0FF' }];
+  barChartData:any[] = [
+  {data: [], label: 'Thoughts'}
+];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public entriesProvider: EntriesProvider
+  ) {
+    this.entriesProvider.getWeeklyThoughts().subscribe((data) => {
+      this.entries = data.week;
+      for (let entry of this.entries) {
+        this.barChartLabels.push(this.weekdays[new Date(entry.date).getDay()]);
+        this.barChartData[0].data.push(entry.amount);
+      }
+      this.isDataAvailable = true;
+      console.log(this.barChartLabels);
+    });
+  }
+
+
+  chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  chartHovered(e:any):void {
+    console.log(e);
   }
 
   ionViewDidLoad() {
