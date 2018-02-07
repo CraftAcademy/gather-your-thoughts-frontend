b41730d31f458aed5dd7d44a9459b14bee857bdc
aljoh <reachingalfred@gmail.com>
2018-02-07 20:59:25 +0100

    removes unused imports

--- src/pages/sentiments-index/sentiments-index.ts
+++ src/pages/sentiments-index/sentiments-index.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SentimentsShowPage } from '../sentiments-show/sentiments-show';
 import { trigger, state, style, animate, transition } from '@angular/animations';
-import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @IonicPage()
 @Component({
